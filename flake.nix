21077407468f68d90fb00cbc4f1e2d05e483cec3
= <=>
2023-06-17 20:41:21 -0400

    Updating to latest

--- flake.nix
+++ flake.nix
@@ -3,22 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       defaultPackage.${system} = home-manager.defaultPackage.${system};
       homeConfigurations.derek = home-manager.lib.homeManagerConfiguration {
-	inherit pkgs;
-
-	pkkgs = nixpkgs.legacyPackages.${system};
+	pkgs = import nixpkgs {
+		inherit system;
+		config.allowUnfree = true;
+	};
 	modules = [
 	  ./home.nix
 	];
